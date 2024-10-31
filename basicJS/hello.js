ed848c9e011014dbf1980be23292f1cb6560ede3
theakhilsarkar18 <akhilsodvadiya7@gmail.com>
2024-10-31 19:30:19 +0530

    ..

--- basicJS/hello.js
+++ basicJS/hello.js
@@ -1,4 +1,8 @@
-// console.log("Hello World")
+// to print messasge in console :
+// console.log("Hello Akhil");
 
-document.write("Hello World");
+// to print message in web page :
+// document.write("Hello Akhil");
 
+// variables in JS :
+// variables are container which can store a value and also we can change it
