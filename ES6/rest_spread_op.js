c53f64571f97ea940cb882cb43578a49669b0d01
RW <prafulpatnecha@gmail.com>
2024-12-05 18:47:12 +0530

    ...

---
+++ ES6/rest_spread_op.js
@@ -0,0 +1,5 @@
+// Rest and Spread Operators (...)
+// The ... operator is used for both rest and spread functionality depending on where it is used.
+
+// Rest Operator
+// The rest operator collects multiple elements into a single array or object.
