7f538b56e1d8ff48c86336ec4e3106a3d9e895af
RW <prafulpatnecha@gmail.com>
2024-12-05 19:15:59 +0530

    ...

---
+++ string/string_fun.js
@@ -0,0 +1,7 @@
+// concat()	Combine strings
+// slice()	Extract part of a string	'Hello World'
+// substring()	Extract part of a string	'Script'
+// replace()	Replace a part of a string	'Hello, JavaScript!'
+// toUpperCase()	Convert to uppercase	'HELLO'
+// trim()	Remove whitespace	'Hello, World!'
+// split()	Split a string into an array	['apple', 'banana']
