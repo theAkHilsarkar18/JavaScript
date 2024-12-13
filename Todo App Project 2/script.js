129302211248b59a5cbcbd17955341132a45a4f2
theAkHilsarkar18 <akhilsodvadiya7@gmail.com>
2024-12-13 16:15:10 +0530

    ...

--- Todo App Project 2/script.js
+++ Todo App Project 2/script.js
@@ -1,29 +1,31 @@
 const button = document.getElementById("button");
 const listDiv = document.getElementById("list");
 
-
-let todoList = JSON.parse(localStorage.getItem("todos")) || [];
+const task = document.getElementById("task")
+let todoList =   [];
 
 button.addEventListener(
     'click',
-    (e) => {
-        const task = document.getElementById("task").value.trim();
-        todoList.push(task);
-        const setList = JSON.stringify(todoList);
-        localStorage.setItem("todos", setList);
+    () => {
+
+        const taskvalue = task.value
+        todoList.push(taskvalue)
+
+        console.log(todoList)
+
     }
+
 );
 
 
-function display() {
-    listDiv.innerHTML = "";
-    todoList.forEach((e) => {
-        const task = document.createElement("div");
-        console.log(e);
-        task.innerHTML = "<p>" + e + " <button type='submit' id='delButton'>Delete</button></p>";
-        listDiv.appendChild(task);
-    });
-}
+// function display( ) {
+//     listDiv.innerHTML = "";
+//     todoList.forEach((e) => {
+//         const task = document.createElement("div");
+//         console.log(e);
+//         task.innerHTML = "<p>" + e + " <button type='submit' id='delButton'>Delete</button></p>";
+//         listDiv.appendChild(task);
+//     });
+// }
 
 
-display();
