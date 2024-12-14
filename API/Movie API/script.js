ab0e35f2fea4aacfd0f011bda40888589f6bae2c
Akhil Sodvadiya <113697861+theAkHilsarkar18@users.noreply.github.com>
2024-12-14 10:05:28 +0530

    Update script.js

--- API/Movie API/script.js
+++ API/Movie API/script.js
@@ -1,18 +1,28 @@
-//https://www.omdbapi.com/?i=tt3896198&apikey=bb11e0e8&t=pushpa
-const button = document.getElementById("button");
+const button = document.getElementById("search");
 
 button.addEventListener(
-    'click',
-    (e) => {
-        const search = document.getElementById("search").value.trim();
-        const api = "https://www.omdbapi.com/?i=tt3896198&apikey=bb11e0e8&t=${search}";
-        fetch(api).then(
-            (response) => {
-                if (!response.ok) {
-                    console.log("API Cant call !");
-                }
-            }
-        );
-
-    }
+  'click',
+  (e) => {
+    const search = document.getElementById("input").value.trim();
+    const api = "https://www.omdbapi.com/?i=tt3896198&apikey=bb11e0e8&t=" + search;;
+    fetch(api).then(
+      (response) => {
+        if (!response.ok) {
+          throw new Error("API Calling Failed !");
+        }
+        return response.json();
+      }
+    ).then(
+      (data) => {
+        console.log(data);
+        const output = document.querySelector(".output");
+        const div = document.createElement("div");
+        div.innerHTML = `
+            <h4>${data.Title}</h4>
+            <img src="${data.Poster}">
+        `;
+        output.appendChild(div);
+      }
+    );
+  }
 );
